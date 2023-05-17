15770ca8951ff813c93b2f74844b8b7c1de3e599
Yesenia <yeseniaaag9@gmail.com>
2023-05-17 16:07:45 -0500

    home page done; no functionality in button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,42 +28,35 @@ function Navbar() {
     }
 
     return (
-        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <nav className="flex items-center flex-wrap bg-red-600 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <Link to="/" className="font-semibold text-xl tracking-tight">Step</Link> 
+                <Link to="/" className="font-semibold text-xl tracking-tight">My App</Link> 
             </div>
             <div className="block">
-                <button onClick={dropDown} className="flex items-center px-3 py-2 text-teal-200 border rounded border-teal-400 hover:text-white hover:border-white">
+                <button onClick={dropDown} className="flex items-center px-3 py-2 text-black border rounded border-black hover:text-white hover:border-white">
                     <i className="fas fa-bars"></i>
                 </button>
             </div>
             { isVisible ? ( 
-                <div className="w-full block flex-grow items-center">
-                    <div className="text-sm lg:flex-grow">
-                        <Button className="p-3 m-5 bg-teal-400 justify-center">
+                <div className="block">
+                    <div className="flex flex-row text-sm lg:flex-grow pl-3">
+                        <Button className="p-3 m-5 bg-black justify-center rounded-lg">
                             <div>
-                                <Link to="/" onClick={ clicked } className="flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                                <Link to="/" onClick={ clicked } className="flex place-items-center mt-4 pl-8 pr-4 lg:inline-block lg:mt-0 text-zinc-400 hover:text-white mr-4">
                                     Home
                                 </Link>
                             </div>
                         </Button>
-                        <Button className="p-3 m-5 bg-teal-400 justify-center">
+                        <Button className="p-3 m-5 bg-black justify-center rounded-lg">
                             <div>
-                                <Link to="/about" onClick={ clicked } className="flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                                    About
+                                <Link to="/whisky" onClick={ clicked } className="flex place-items-center mt-4 ml-3 pl-5 pr-5 lg:inline-block lg:mt-0 text-zinc-400 hover:text-white mr-4">
+                                    Whisky
                                 </Link>
                             </div>
                         </Button>
-                        <Button className="p-3 m-5 bg-teal-400 justify-center">
+                        <Button className="p-3 m-5 bg-black justify-center rounded-lg">
                             <div>
-                                <Link to="/sneaker" onClick={ clicked } className="flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                                    Shoe
-                                </Link>
-                            </div>
-                        </Button>
-                        <Button className="p-3 m-5 bg-teal-400 justify-center">
-                            <div>
-                                <Link to="/dashboard" onClick={ clicked } className="flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                                <Link to="/dashboard" onClick={ clicked } className="flex place-items-center mt-4 pr-1 pl-5 lg:inline-block lg:mt-0 text-zinc-400 hover:text-white mr-4">
                                     Dashboard
                                 </Link>
                             </div>
@@ -71,18 +64,18 @@ function Navbar() {
                         {
                             !auth.currentUser ?
 
-                            <Button className="p-3 m-5 bg-teal-400 justify-center">
+                            <Button className="p-3 m-5 bg-black justify-center rounded-lg">
                                 <div>
-                                    <Link to="/" onClick={ () => { signInOnClick()}} className="flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                                    <Link to="/" onClick={ () => { signInOnClick()}} className="flex place-items-center mt-4 pl-7 pr-4 lg:inline-block lg:mt-0 text-zinc-400 hover:text-white">
                                         Login
                                     </Link>
                                 </div>
                             </Button>
                             :
-                            <Button className="p-3 m-5 bg-teal-400 justify-center">
+                            <Button className="p-3 m-5 bg-black justify-center rounded-lg">
                                 <div>
-                                    <Link to="/" onClick={ () => { signOutOnClick()}} className="flex place-items-center mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                                        Sign Out
+                                    <Link to="/" onClick={ () => { signOutOnClick()}} className="flex place-items-center mt-4 pl-8 pr-8 lg:inline-block lg:mt-0 text-zinc-400 hover:text-white">
+                                        Logout
                                     </Link>
                                 </div>
                             </Button>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,29 @@
 import redWhiteBlack from '../assets/images/red-white-black.jpg'
+import white from '../assets/images/white.jpg'
+import pink from '../assets/images/pink.jpg'
+import Button from '../components/Button'
+import { Link } from 'react-router-dom'
+import signInOnClick from '../components/DataTable'
+import { auth } from '../config/firebase'
+
 
 function Home() {
   return (
     <div>
-      <div className="bg-image img1"></div>
-      <div className="bg-image img2"></div>
-      <div className="bg-image img3"></div>
+      <div style={{ backgroundImage: `url(${ redWhiteBlack })`, height: 1000 }} 
+      className="mx-auto bg-cover"></div>
 
       <div className="bg-text">Sneakerhead?</div> 
+
+      <div className="flex justify-center bg-stone-600 p-4">
+      {
+        <Button className="p-3 pl-14 pr-14 bg-red-600 m-4 font-semibold rounded hover:bg-slate-800 text-white hover:text-white">
+          <Link to="/" onClick={ () => { signInOnClick()}}>
+            Join Now
+          </Link>
+        </Button>
+      }
+      </div>
     </div>
   )
 }
