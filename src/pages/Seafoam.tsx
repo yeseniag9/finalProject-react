74b937eacda6e66c25d2184b83184ce1647d8e38
Yesenia <yeseniaaag9@gmail.com>
2023-05-17 22:31:32 -0500

    seafoam page done

--- src/pages/Seafoam.tsx
+++ src/pages/Seafoam.tsx
@@ -4,8 +4,15 @@ function Seafoam() {
   return (
     <div>
         <div className="flex justify-center pt-14 mt-14 text-7xl font-sans font-semibold">Air Jordan 4 'Seafoam'</div>
-        <img className="h-auto max-w-full" src={seafoam} alt="Air Jordan 4 Seafoam" />
-        <h1 className="flex justify-start text-3xl font-sans font-semibold pl-3">2023</h1>
+
+        <div className="pt-1">
+            <div className="pt-14 mt-6">
+                <img className="mx-auto seafoam pt-14 mt-14" src={seafoam} alt="Air Jordan 4 Seafoam" />
+                <div className="pt-5">   
+                    <h1 className="flex justify-start text-3xl font-sans font-semibold pl-3 pt-14 mt-14">2023</h1>
+                </div>
+            </div>
+         </div>
     </div>
   )
 }
