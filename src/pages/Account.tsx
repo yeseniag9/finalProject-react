5a4982624b528cf6a7f4da67bcdaec0eefd03908
Yesenia <yeseniaaag9@gmail.com>
2023-07-18 21:37:04 -0500

    added title to account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,7 +1,8 @@
 function Account() {
     return (
         <div>
-            
+            <h1 className='flex justify-left pl-14 ml-14 pt-14 mt-14 text-7xl font-sans font-medium'>Hey there,</h1>
+            <h2 className='flex justify-left pl-14 ml-14 pt-2 mt-3 text-3xl font-sans'>Welcome back! Here's what's been added since you've been away.</h2>
         </div>
     )
 }
